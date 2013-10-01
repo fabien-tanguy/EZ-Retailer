6094c35f7d36e3098b8e978e0a57ea95f55188c3
fabientanguy <fabientanguy@free.fr>
2013-10-02 00:17:11 +0200

    rescale photos 3

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -32,7 +32,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
-  process :resize_to_fill => [640,640]
+  process :resize_to_fit => [640,640]
   
   #
   # def scale(width, height)
