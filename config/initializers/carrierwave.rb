fb2dea77254cbefdafc5f5a62220861441c93eb2
fabientanguy <fabientanguy@free.fr>
2013-10-15 13:04:25 +0200

    modif logo

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
     config.aws_bucket       = 'm-adw'                  
 
     config.aws_credentials  = {
-      access_key_id: 'YOUR_ACCESS_KEY_ID',
-      secret_access_key: 'YOUR_SECRET_ACCESS_KEY'
+      access_key_id: 'AKIAJUUUV3XHQOEH77HQ',
+      secret_access_key: 'B9IY/ZKhoVklF48iRuvgRvsm4S0Ow+qPPlQTPiGK'
     }
 end
