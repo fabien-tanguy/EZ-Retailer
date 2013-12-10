32bda1c69dbe0c0b240f72dad31f453a7071aa9e
fabientanguy <fabientanguy@free.fr>
2013-12-10 11:19:46 +0100

    modif de l'ajout de photo dans panel

--- app/assets/javascripts/admin/photos_to_add.js
+++ app/assets/javascripts/admin/photos_to_add.js
@@ -1,3 +1,7 @@
-$(document).on('pageinit',function() { 
+$(document).on('pageinit',function() {
 	
+    // select the good photo 
+	//$('.choose_from_album').click(function(){
+	//	$('.choose_from_album').removeClass("photo-selected");
+	//	$(this).addClass("photo-selected");
 });
